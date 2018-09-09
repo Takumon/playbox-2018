3cfa8f984c58bbdd46e800b9235714a3a7889c78
takumon <inouetakumon@gmail.com>
2018-09-09 21:41:30 +0900

    UdemyのReact Redux講座2章終了時点

---
+++ ReduxSimpleStarter/src/components/video_detail.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+class VideoDetail extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render () {
+    if (!this.props.video) {
+      return <div>Loading...</div>
+    }
+
+    const title = this.props.video.snippet.title;
+    const description = this.props.video.snippet.description;
+    const videoId = this.props.video.id.videoId;
+    const url = "https://www.youtube.com/embed/" + videoId;
+
+    return (
+      <div className="video-detail col-md-8">
+        <div className="embed-responsive embed-responsive-16by9">
+          <iframe className="embed-responsive-item" src={url}></iframe>
+        </div>
+
+        <div className="details">
+          <div>{title}</div>
+          <div>{description}</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default VideoDetail;
